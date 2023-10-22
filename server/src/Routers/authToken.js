458c401e6c03fb4dd52474622aadd624db4bcb89
Vromaniac <115129648+Vromaniac@users.noreply.github.com>
2023-10-22 13:17:29 -0400

    Update authToken.js

--- server/src/Routers/authToken.js
+++ server/src/Routers/authToken.js
@@ -1,12 +1,17 @@
 import jwt from "jsonwebtoken";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+const JWTkey = process.env.jwtkey;
+
 const authenticateToken = (req, res, next) => {
   const token = req.header('Authorization');
   if (!token){ 
     console.log('No token provided');
     return res.status(401).json({ message: 'Access denied' });}
 
-  jwt.verify(token, 'your-secret-key', (err, user) => {
+  jwt.verify(token, JWTkey, (err, user) => {
     if (err) {
       console.log(`error verifying: ${err}`);
       return res.status(403).json({ message: 'Invalid token' });}
